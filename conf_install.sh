b1b234738f417ead9d66bf28ddc59f4e9c4c83a2
alexis boissiere <alexis.boissiere@epita.fr>
2019-11-11 14:46:13 +0100

    [Update] tree of old_conf

--- conf_install.sh
+++ conf_install.sh
@@ -3,71 +3,82 @@
 install() 
 {
 	echo "Beginning $1 install..."
+	str="OS detected:"
+
 	if [ -f /etc/debian_version ]; then
-		echo "we have detected that your os is based on debian"
+		echo "$str Debian"
 		packet="apt-get install -y"
 	elif [ -f /etc/arch-release ]; then
-		echo "we have detected that your os is based on arch"
+		echo "$str Arch"
 		packet="pacman -Suy"
 	else
 		echo -e "\e[31Your distro not supported for now"
 		exit 1
 	fi
 
-
 	sudo $packet $1
 	if [ $1 = "zsh" ]; then
 		sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	fi
-	
 };
 
 declare -a list_command
-list_command=("bash" "zsh" "neofetch" "i3" "dtgaga")
+list_command=("bash" "zsh" "neofetch" "i3" "vim")
 
-#Check if the required module are install and install them if not"
-for com in "${list_command[@]}"
-do
-	if command -v $com; then
-		echo -e "[\e[32mOK\e[39m] $com"
-	else
-		echo -e "[\e[31mKO\e[39m] $com"
-		echo "$com is not install on your system, would you like to install it ? (yes/no)"
-		read -p "" user_choice
+check()
+{
 
-		if [ $user_choice = "yes" ]; then
-			install $com
-		elif [ $user_choice = "no" ]; then
-			echo -e "\e[31mFor now the install.sh can not continue without $com"
-			exit 1
+	#Check if the required module are install and install them if not"
+	for com in "${list_command[@]}"
+	do
+		if command -v $com; then
+			echo -e "[\e[32mOK\e[39m] $com"
 		else
-			echo -e "\e[31mYou did not provided yes or no answer"
-			exit 1
-		fi
-	fi
-done
+			echo -e "[\e[31mKO\e[39m] $com"
+			echo "$com is not install on your system, would you like to install it ? (y/n)"
+			read -p "" user_choice
 
+			if [ $user_choice = "y" ]; then
+				install $com
+			elif [ $user_choice = "n" ]; then
+				echo -e "\e[31mFor now the install.sh can not continue without $com"
+				exit 1
+			else
+				echo -e "\e[31mYou did not provided yes or no answer"
+				exit 1
+			fi
+		fi
+	done
+};
 
 declare -a list_conf_file
-list_conf_file=(".bashrc" 
-				".zshrc"
-				".config/neofetch/config.conf"
-				".config/i3/config")
+list_conf_file=(".bashrc" ".zshrc" ".config/neofetch/config.conf"
+				".config/i3/config" ".vimrc")
+cp_old_conf()
+{
 
-mkdir old_config
-cd old_config
+	mkdir old_config
+	cd old_config
 
-for file in "${list_conf_file[@]}"
-do
-	if [ -f $HOME/$file ]; then
-		echo -e "\e[32mcopying $HOME/$file into $PWD"
-		cp $HOME/$file $PWD
-	else
-		echo -e "\e[31m$HOME/$file does not exit"
-		echo "Please install what is needed in order to have a $file file"
-	fi
-done
+	for i in "${!list_conf_file[@]}"
+	do
+		file="${list_conf_file[$i]}"
+		folder="${list_command[$i]}"
+		if [ -f $HOME/$file ]; then
+			echo -e "\e[32mcopying $HOME/$file into $PWD/$folder"
+			mkdir $PWD/$folder
+			cp $HOME/$file $PWD/$folder
+		else
+			echo -e "\e[31m$HOME/$file does not exit"
+			echo "Please install what is needed in order to have a $file file"
+		fi
+	done
+};
+
+check
+cp_old_conf
+
 
 tree -a 
 cd ..
-#rm -rf old_config
+rm -rf old_config
