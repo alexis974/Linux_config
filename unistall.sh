fda2330b9b5b9a3b29f4ebcb920228ef4fe85ead
alexis boissiere <alexis.boissiere@epita.fr>
2019-11-11 17:49:38 +0100

    [Add] working unistall script

--- unistall.sh
+++ unistall.sh
@@ -1,4 +1,36 @@
-if [ -d $PWD/old_config ]; then
-	cp .bashrc $HOME
-	cp .zshrc $HOME
+#!/usr/bin/env bash
+
+if [ ! -d $PWD/old_config ]; then
+	echo -e "\e[31mMissing directory: "old_config"\e[39m"
+	exit 1
 fi
+
+
+declare -a list_command
+list_command=("bash" "zsh" "neofetch" "i3")
+
+declare -a list_conf_file
+list_conf_file_location=("" "" ".config/neofetch/" ".config/i3/")
+
+declare -a list_conf_file
+list_conf_file=(".bashrc" ".zshrc" "config.conf" "config")
+
+declare -i nb_config_file
+nb_config_file="${#list_conf_file[@]}"
+
+for ((i=0; i<${nb_config_file}; i++)); do
+	file="${list_conf_file_location[$i]}""${list_conf_file[$i]}"
+	folder="${list_command[$i]}"
+
+	if [ -L $HOME/$file ]; then
+		echo -e "\e[96mRemoving symbolic link:\e[39m $HOME/$file"
+		rm $HOME/$file
+
+		echo -e "\e[32mCopying old config file\e[39m $PWD/old_config/$folder/"${list_conf_file[$i]}""
+		cp $PWD/old_config/$folder/"${list_conf_file[$i]}" $HOME/$file
+	else
+		echo -e "\e[31mNo symbolic link found for $folder\e[39m"
+	fi
+done
+
+rm -rf old_config
