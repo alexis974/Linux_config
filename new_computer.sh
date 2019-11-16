07abf313e572f459703ca2cd08febdd68f1ffcbe
alexis boissiere <alexis.boissiere@epita.fr>
2019-11-16 20:13:08 +0100

    [Update] Script now continue installation even if it could not install a package

--- new_computer.sh
+++ new_computer.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+clear
 printf "\e[96m\e[4mStarting installation:\e[0m\n"
 
 str="OS detected:"
@@ -10,25 +11,30 @@ declare -a list_package
 if [ -f /etc/debian_version ]; then
 	echo "$str Debian"
 	package_manager="apt install -y"
-	list_package=("git" "vim" "wget" "curl" "zsh" "neofetch" "i3" "evince" 
+	list_package=("git" "vim" "snap" "wget" "curl" "zsh" "neofetch" "i3" "evince" 
 		"thunar" "darktable" "vlc" "firefox"
 		"chromium-browser" "okular" "gimp" "htop" "audacity" "steam" "geany"
 		"ranger")
-	printf "\e[96mBeginning system update\e[0m"
+	printf "\e[96mBeginning system update\e[0m\n"
 	sudo apt update
 	sudo apt upgrade
+	printf "\e[96mEnd of update\e[0m\n\n"
 elif [ -f /etc/arch-release ]; then
 	echo "$str Arch"
 	package_manager="pacman -S --noconfirm"
-	list_package=("git" "vim" "wget" "curl" "zsh" "neofetch" "i3" "evince" 
+	list_package=("git" "vim" "snap" "wget" "curl" "zsh" "neofetch" "i3" "evince" 
 		"thunar" "darktable" "vlc" "firefox" "chromium"
 		"okular" "gimp" "htop" "audacity" "steam" "geany" "ranger")
-	printf "\e[96mBeginning system update\e[0m"
+	printf "\e[96mBeginning system update\e[0m\n"
 	sudo pacman -Syu
+	printf "\e[96mEnd of update\e[0m\n\n"
 else
 	echo -e "\e[31Your distro not supported for now"
 	exit 1
 fi
+
+
+printf "\e[96mChecking package:\e[0m\n"
 
 
 #Check if the required module are install and install them if not"
@@ -39,18 +45,18 @@ do
 		printf "[\e[32mOK\e[0m] \e[32m$package\e[0m\n" >> tmp.txt
 	else
 		printf "[\e[31mKO\e[0m] \e[31m$package\e[0m\n"
-		printf "$package has not been found. Starting installation...\n" 
+		printf "\e[96m$package has not been found. Starting installation...\n" 
 		sudo $package_manager $package #> /dev/null 2>&1
 		if [ $package = "zsh" ]; then
 			sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 		fi
 
 		if command -v $package > /dev/null 2>&1; then
-			printf "[\e[32mOK\e[0m] \e[32m$package\e[0m\n"
+			printf "[\e[32mOK\e[0m] \e[32m$package (NEW)\e[0m\n" 
 			printf "[\e[32mOK\e[0m] \e[32m$package (NEW)\e[0m\n" >> tmp.txt
 		else
 			printf "\e[31mCould not install $package\e[0m\n"
-			exit 1
+			printf "[\e[31mKO\e[0m] \e[31m$package\e[0m\n" >> tmp.txt
 		fi
 
 	fi
